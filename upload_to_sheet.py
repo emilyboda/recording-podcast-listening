36e5b41aa9db9190d3bfb421e127ed057c40ade0
emilyboda <emily.boda@gmail.com>
2020-06-29 10:39:23 -0400

    added credentials and token file path

--- upload_to_sheet.py
+++ upload_to_sheet.py
@@ -7,7 +7,7 @@ from google.auth.transport.requests import Request
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-def auth():
+def auth(credentials_file_path):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
@@ -15,8 +15,8 @@ def auth():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(credentials_file_path+'token.pickle'):
+        with open(credentials_file_path+'token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -24,10 +24,10 @@ def auth():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                credentials_file_path+'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(credentials_file_path+'token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
